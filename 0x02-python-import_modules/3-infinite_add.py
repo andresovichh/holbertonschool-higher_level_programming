d5d16c096aeb1d0c9c07face083e3acb3313930e
andresovichh <ahenderson.holberton@gmail.com>
2021-12-23 16:47:44 -0300

    test

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
+
     i = 0
-    for argst in range(len(sys.argv) - 1):
-        i += int(sys.argv[argst + 1])
+    for argst in range(1, len(sys.argv)):
+        i += int(sys.argv[argst])
         print("{}".format(i))
