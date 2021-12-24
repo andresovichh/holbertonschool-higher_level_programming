6d2e58fa77a70de64236dbb75b92eb29b3786943
andresovichh <ahenderson.holberton@gmail.com>
2021-12-23 23:00:17 -0300

    test

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,2 +1,7 @@
 #!/usr/bin/python3
-if __name__ == "__main__"
+if __name__ == "__main__":
+    import hidden_4
+
+    for h in dir(hidden_4):
+        if h[0] != '_' and h[1] != '_':
+            print(h)
