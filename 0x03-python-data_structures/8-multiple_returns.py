068680f7717dbd2fe6768cd1e11ab69030ceae1f
andresovichh <ahenderson.holberton@gmail.com>
2022-01-03 17:32:05 -0300

    test

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,7 +2,7 @@
 def multiple_returns(sentence):
     if not sentence:
         a_tuple = 0, None
-        return a_tuple  
+        return a_tuple
     else:
         a_tuple = len(sentence), sentence[0]
         return a_tuple
