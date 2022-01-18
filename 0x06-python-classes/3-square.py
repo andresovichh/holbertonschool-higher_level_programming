6071b1cbd2b65bb889713e2cf414598801e190ee
andresovichh <ahenderson.holberton@gmail.com>
2022-01-18 20:41:39 -0300

    test

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+""" define a class Square
+das
+"""
+
+
+class Square:
+    """ this is the Square
+    class
+    """
+    def __init__(self, size=0):
+        """ blah blas cajs"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+    def area(self):
+        self.area_2 = self.__size * self.__size
+        return(self.area_2)
