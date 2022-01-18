e35987c423ddfc4b57fa8521d9f7ff62688c9bc1
andresovichh <ahenderson.holberton@gmail.com>
2022-01-18 20:58:49 -0300

    test

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+""" define a class Square
+das
+"""
+
+
+class Square:
+    """ this is the Square
+    class
+    """
+    def __init__(self, size=0):
+        """ blah blas cajs"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+    @property
+    def size(self):
+        """ Tghis whould be the getter"""
+        return(self.__size)
+
+    @size.setter
+    def size(self, value):
+        """ this would be the setter"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """REturn area"""
+        return self.__size * self.__size
