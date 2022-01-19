bd32399203da309f8e5cca3fd41a7399be47b0da
andresovichh <ahenderson.holberton@gmail.com>
2022-01-18 21:41:23 -0300

    test

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,69 @@
+#!/usr/bin/python3
+
+""" define a class Square
+das
+"""
+
+
+from ctypes import sizeof
+
+
+class Square:
+    """ this is the Square
+    class
+    """
+    def __init__(self, size=0):
+        """ blah blas cajs"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    @property
+    def size(self):
+        """ Tghis whould be the getter"""
+        return(self.__size)
+
+    @size.setter
+    def size(self, value):
+        """ this would be the setter"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """REturn area"""
+        return self.__size * self.__size
+
+    def my_print(self):
+        """ prints a square"""
+        if self.__size == 0:
+            print("\n", end="")
+        else:
+            for i in range(self.__size):
+                for i in range(self.__size):
+                    print("#", end="")
+                print()
+
+    
+@property
+def position(self):
+    """ Tghis whould be the getter"""
+    return(self.__size)
+
+@position.setter
+def position(self, value):
+    """ this would be the setter"""
+    if (type(value) is not tuple) or (if i in value != 2) :
+        raise TypeError("position must be a tuple of 2 positive integers")
+    elif value < 0:
+        raise ValueError("size must be >= 0")
+    else:
+        self.__size = value
+
+def __init__(self, size=0, position=(0, 0)):
