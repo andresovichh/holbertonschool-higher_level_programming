adb2981af68c59dbc674ff266c64913e36190570
andresovichh <ahenderson.holberton@gmail.com>
2022-01-18 22:56:24 -0300

    test

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,13 +1,3 @@
 #!/usr/bin/python3
 
-""" define a class Square
-das
-"""
 
-
-class Square:
-    """ this is the Square
-    class
-    """
-    def __init__(self, size):
-        self.__size = size
