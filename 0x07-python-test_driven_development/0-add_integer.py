227d5da49fd121cb0f6e9402969aa8a812bcc161
andresovichh <ahenderson.holberton@gmail.com>
2022-01-23 17:51:24 -0300

    test

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -2,22 +2,14 @@
 
 """
 This module adds integers, with one foo, add_integer()
-For example,
 >>> add_integer(30, 5)
 35
 """
 
 def add_integer(a, b=98):
     """ Return the addition of two integers
-
-    Args:
-        a (int): an int
-        b (int): another int
-
-    Returns:
-        An int with the addition
-
-    """
+    a (int): an int
+    b (int): another int"""
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     
