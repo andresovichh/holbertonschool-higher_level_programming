a032356cb2ed95a84d6e6b298c96c3736b196439
andresovichh <ahenderson.holberton@gmail.com>
2022-01-24 09:36:58 -0300

    test 3 works

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,21 @@
+#!usr/bin/python3
+"""
+A foo that takes two args and prints
+My name is <first name> <last name> to stdout
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    say_my_name: print name to stdout
+    Args
+    first_name (str): str to prt name
+    last_name (str): str to prt last name
+    Error:
+    if either last_name or first name not str: TypeError
+    """
+    if not (isinstance(first_name, str)):
+        raise TypeError("first_name must be a string")
+    if not (isinstance(last_name, str)):
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
