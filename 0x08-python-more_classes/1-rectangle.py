6d007e51c5511c6b709136c3ea63b997b8d671d8
andresovichh <ahenderson.holberton@gmail.com>
2022-01-24 18:33:49 -0300

    test

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,40 @@
+#!usr/bin/python3
+""" A module with a class for Rectangle"""
+
+from turtle import width
+
+
+class Rectangle:
+    """Rectangle  is
+    created here"""
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+        if not isinstance(width, (int, float)):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        if not isinstance(width, (int, float)):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+
+    @property
+    def height(self):
+        return self.__height
+    
+
