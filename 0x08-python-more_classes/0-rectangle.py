588cf4069f04f3681c4e01691e63f358d2158e47
andresovichh <ahenderson.holberton@gmail.com>
2022-01-24 18:10:10 -0300

    test

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,12 +1,10 @@
 #!usr/bin/python3
-"""
-Rectangle Module Class
+"""Rectangle Module Class
 """
 
 
 class Rectangle:
-    """
-    Definition of rectangle
+    """Definition of rectangle
     """
     def __init__(self):
         self
