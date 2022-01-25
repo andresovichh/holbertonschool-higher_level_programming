e6cac69935344541ec8497986b2d8bce3b4b70f2
andresovichh <ahenderson.holberton@gmail.com>
2022-01-24 23:11:23 -0300

    test

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -87,6 +87,7 @@ class Rectangle:
 
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
+        """checks whether 2 rectangles have same area"""
         if not isinstance(rect_1, Rectangle):
             raise TypeError("rect_1 must be an instance of Rectangle")
         if not isinstance(rect_2, Rectangle):
