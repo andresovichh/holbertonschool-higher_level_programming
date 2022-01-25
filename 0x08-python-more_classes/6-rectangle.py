999d7689a78c80cb0eb8e818eaf4bd10a6a31b45
andresovichh <ahenderson.holberton@gmail.com>
2022-01-24 22:38:57 -0300

    test

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -21,7 +21,7 @@ class Rectangle:
         self.__width = width
         self.__height = height
         # to count how many instances
-        number_of_instances += 1
+        Rectangle.number_of_instances += 1
 
     def __str__(self):
         a_str = []
