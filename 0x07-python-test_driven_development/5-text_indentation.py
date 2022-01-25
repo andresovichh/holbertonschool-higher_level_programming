e658404d1193263534865ab026786d5478135466
andresovichh <ahenderson.holberton@gmail.com>
2022-01-25 19:53:30 -0300

    test

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -10,11 +10,13 @@ def text_indentation(text):
     if not(isinstance(text, str)):
         raise TypeError("text must be a string")
     
-    res = text.split()
-    dott = "."
-    for i in res:
-        for x in i:
-            if(x == "."):
-                print("\n")
-            else:
-                print(i, end=" ")
+    # res = text.split()
+    # dott = "."
+    # for i in res:
+    #     if x in range(len(i)) == ".":
+    #         print("\n")
+    for delim in ".:?":
+        text = (delim +"\n\n").join(
+            [line.strip(" ") for line in text.split(delim)])
+
+    print("{}".format(text), end="")
