ca01a7555fc2f139fd156b2e8bc726a48a20596e
andresovichh <ahenderson.holberton@gmail.com>
2022-01-24 21:41:43 -0300

    test

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+""" A module with a class for Rectangle
+"""
+
+
+class Rectangle:
+    """Rectangle  is
+    created here"""
+
+    def __init__(self, width=0, height=0):
+        """ starting instance"""
+        if not isinstance(width, (int, float)):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        if not isinstance(height, (int, float)):
+            raise TypeError("height must be an integer")
+        if height < 0:
+            raise ValueError("height must be >= 0")
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """ gest the value of`width`"""
+        return self.__width
+
+    @property
+    def height(self):
+        """ Gets  the value of `height`"""
+        return self.__height
+
+    @width.setter
+    def width(self, value):
+        """ sets the value of `width`"""
+        if not isinstance(value, (int, float)):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @height.setter
+    def height(self, value):
+        """ sets  the value of `height`"""
+        if not isinstance(value, (int, float)):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return (self.__height * self.__width)
+    
+    def perimeter(self):
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return (2 * (self.__height + self.__width))
