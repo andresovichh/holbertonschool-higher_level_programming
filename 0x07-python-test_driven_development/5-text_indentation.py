af22182b91071d82bb6f103a96b8452125d04059
andresovichh <ahenderson.holberton@gmail.com>
2022-01-26 13:31:38 -0300

    test

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -13,7 +13,7 @@ def text_indentation(text):
     if not(isinstance(text, str)):
         raise TypeError("text must be a string")
     for delim in ".:?":
-        text = (delim + "\n\n").join(
-            [line.strip(" ") for line in text.split(delim)])
+        text = (delim + "\n\n").join\
+            ([line.strip(" ") for line in text.split(delim)])
 
     print("{}".format(text), end="")
