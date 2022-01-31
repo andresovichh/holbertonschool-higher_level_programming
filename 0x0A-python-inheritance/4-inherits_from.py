2d1287414ab5d8d195ae80abd2f7eacad79ca9c9
andresovichh <ahenderson.holberton@gmail.com>
2022-01-31 16:36:41 -0300

    test

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
+"""
+Module that checks if the obj is an instance of a class that inherited
+(directly or indirectly) from the specified class """
 
 def inherits_from(obj, a_class):
-    if type(obj) == a_class:
+    """
+    Method that returns true or false
+    """
+    if issubclass(type(obj), a_class):
         return True
     else:
         return False
