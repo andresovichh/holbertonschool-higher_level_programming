350176811db8743aa448aa2784e3261b9259f579
andresovichh <ahenderson.holberton@gmail.com>
2022-02-01 19:12:35 -0300

    test

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,8 +2,11 @@
 """
 returns the JSON representation of an object (string):"""
 
+
 from sys import argv
 """ import argv module"""
+
+
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
