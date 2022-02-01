239c75230af22714f9086451e940dc2cd0a4c7e5
andresovichh <ahenderson.holberton@gmail.com>
2022-02-01 18:18:19 -0300

    test

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,8 +6,6 @@ Module that reads a text file UTF8"""
 
 def read_file(filename=""):
     """ Reads a file with 'with'
-    Args:
-        filename (str): filename to open
     """
     with open(filename, encoding='utf-8') as f:
-        print(f.read())
+        print(f.read(), end="")
