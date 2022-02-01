4367a1b54a93589ebe7dc8d2be8419ee7d7dce79
andresovichh <ahenderson.holberton@gmail.com>
2022-02-01 19:14:23 -0300

    test

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -12,7 +12,7 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 try:
-    f = load_from_json_file("add_item_json" + argv[1:])
-    save_to_json_file(f, "add_item_json")
+    f = load_from_json_file("add_item.json" + argv[1:])
+    save_to_json_file(f, "add_item.json")
 except:
     something
