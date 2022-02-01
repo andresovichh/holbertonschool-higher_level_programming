41298418aeb51eac32be7da58ccc52522975d897
andresovichh <ahenderson.holberton@gmail.com>
2022-02-01 18:54:39 -0300

    tst

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+returns the JSON representation of an object (string):"""
+
+from sys import argv
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+f = load_from_json_file("add_item_json")
+save_to_json_file(argv[0], "add_item_json")
