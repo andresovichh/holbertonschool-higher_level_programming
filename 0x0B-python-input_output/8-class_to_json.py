789337290d476e8e22e9f9a75b881e25ed1dcfcd
andresovichh <ahenderson.holberton@gmail.com>
2022-02-01 21:15:44 -0300

    test

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Module that returns the dictionary description with simple data 
+Module that returns the dictionary description with simple data
 structure (list, dictionary, string,
 integer and boolean) for JSON serialization of an object:"""
 
