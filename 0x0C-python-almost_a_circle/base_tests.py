d4f7971a6e207723d445811244de4d14f41838d5
andresovichh <ahenderson.holberton@gmail.com>
2022-02-05 17:30:22 -0300

    test

--- 0x0C-python-almost_a_circle/base_tests.py
+++ 0x0C-python-almost_a_circle/base_tests.py
@@ -17,6 +17,14 @@ class TestBase(unittest.TestCase):
         """documentation"""
 
         self.assertTrue(len(Base.__doc__) > 0)
+    def test_usual_case(self):
+        """ Basic test case"""
+        b1 = Base()
+        self.assertEqual(b1.id, 1)
+        b2 = Base(12)
+        self.assertEqual(b2.id, 12)
+        b3 = Base(-200)
+        self.assertEqual(b3.id, -200)
     
     def test_documentations(self):
         """check if method has doc"""
@@ -25,12 +33,21 @@ class TestBase(unittest.TestCase):
 
 
 
-    def test_pep8_conformance(self):
-        """Test that we conform to PEP8."""
-        pep8style = pep8.StyleGuide(quiet=True)
-        result = pep8style.check_files(['base.py'])
-        self.assertEqual(result.total_errors, 0, "Found errors")
 
-    #def test_ 
+class TestCodeFormat(unittest.TestCase):
+    def test_pep8_conformance(self):
+        # """Test that we conform to PEP8."""
+        # pep8style = pep8.StyleGuide(quiet=True)
+        # result = pep8style.check_files(['base.py'])
+        # print(result)
+        # self.assertEqual(result.total_errors, 0,
+        #                  "Found code style errors (and warnings).")
+        # https://pep8.readthedocs.io/en/release-1.7.x/intro.html
+        fchecker = pep8.Checker('models/base.py', show_source=True)
+        file_errors = fchecker.check_all()
+
+        print("Found %s errors (and warnings)" % file_errors)
+
+
 if __name__ == "__main__":
     unittest.main()
---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from models.base import Base
+
+
+class Rectangle(Base):
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+
+        self.id = Base(id)
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+
+
