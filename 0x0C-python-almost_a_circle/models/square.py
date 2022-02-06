319795a06c73ac966da9e4acfda7ab67a03c1de4
andresovichh <ahenderson.holberton@gmail.com>
2022-02-06 18:09:25 -0300

    added 12 and __str__ method

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -10,6 +10,11 @@ class Square(Rectangle):
         """ instanciation of a Square object"""
 
         super().__init__(size, size, x, y, id)
+    
+    def __str__(self):
+        """the overloading str method """
+        return '[Square] ('+str(self.id)+') '+str(self.x)+'/'+str(self.y)+\
+            ' - '+str(self.size)
 
     @property
     def size(self):
@@ -21,6 +26,22 @@ class Square(Rectangle):
         """ the size setter"""
         self.width = size
         self.height = size
+    
+    def update(self, *args, **kwargs):
+        """ update the data"""
+        if len(args) > 0:
+            for i in range(len(args)):
+                if i == 0:
+                    self.id = args[0]
+                if i == 1:
+                    self.width = args[1]
+                if i == 2:
+                    self.x = args[2]
+                if i == 3:
+                    self.y = args[3]
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
 
 if __name__ == '__main__':
     main()
