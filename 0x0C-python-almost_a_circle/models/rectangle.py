ff5f9df353267d8da936fa67387c5472bc895840
andresovichh <ahenderson.holberton@gmail.com>
2022-02-07 13:42:26 -0300

    updated > sign height REctangle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -41,7 +41,7 @@ class Rectangle(Base):
         if type(height) is not int:
             raise TypeError("height must be an integer")
         elif height <= 0:
-            raise ValueError("height must be >= 0")
+            raise ValueError("height must be > 0")
 
     @property
     def x(self):
