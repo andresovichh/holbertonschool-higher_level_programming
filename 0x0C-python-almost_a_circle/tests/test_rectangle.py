1355917c2d103222765408f58236fc64837e76d9
andresovichh <ahenderson.holberton@gmail.com>
2022-02-07 15:51:27 -0300

    adding tests to Rectangle

--- 0x0C-python-almost_a_circle/tests/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_rectangle.py
@@ -50,4 +50,37 @@ class TestBase(unittest.TestCase):
         """check if method has doc"""
 
         self.assertTrue(len(Rectangle.update.__doc__) > 0)    
-    
+
+# 1 Test if same result as intranet
+    def test_Rectanglecreation(self):
+        """ as per itnranet"""
+
+        Base._Base__nb_objects = 0
+
+        r1 = Rectangle(10, 2)
+        self.assertEqual(r1.id, 1)
+
+        r2 = Rectangle(2, 10)
+        self.assertEqual(r2.id, 2)
+
+        r3 = Rectangle(10, 2, 0, 0, 12)
+        self.assertEqual(r3.id, 12)
+
+    def test_toomanyargs(self):
+        """ too many args test"""
+        Base._Base__nb_objects = 0
+
+        
+        errm = "TypeError: __init__() takes from 3 to 6 positional arguments but 7 were given"
+        with self.assertRaises(TypeError) as err:
+            rect = Rectangle(1, 2, 3, 4, 5, 6)
+            self.assertEqual(errm, str(err.exception))
+
+    def test_noargs(self):
+        """ no args test"""
+        Base._Base__nb_objects = 0
+        errm = "TypeError: __init__() takes from 3 to 6 positional arguments but 7 were given"
+        with self.assertRaises(TypeError):
+            rec4t = Rectangle()
+   
+
