e73ee0129a4a24d0f7f613d1bf4c7f032628d54b
andresovichh <ahenderson.holberton@gmail.com>
2022-02-07 18:12:58 -0300

    test

--- 0x0C-python-almost_a_circle/tests/test_square.py
+++ 0x0C-python-almost_a_circle/tests/test_square.py
@@ -3,7 +3,6 @@
 import json
 import unittest
 from models.square import Square
-from models.base import Base
 import pep8
 
 class TestBase(unittest.TestCase):
@@ -49,17 +48,6 @@ class TestBase(unittest.TestCase):
         msg = "[Square] (1) 0/0 - 5"
         self.assertEqual(str(s1), msg)
         self.assertEqual(s1.area(), 25)
-    
-    def test_update(self, *args, **kwargs):
-        """ test the update method"""
-        Base._Base__nb_objects = 0
-        s1 = Square(5)
-        s1.update(1, 2, 3, 4)
-        msg = "[Square] (1) 3/4 - 2"
-        self.assertEqual(s1.__str__, msg)
-        s1.update(size=7, y=1)
-        mssg2 = "[Square] (1) 12/1 - 7"
-        self.assertEqual(s1.__str__, mssg2)
 
 
 class TestCodeFormat(unittest.TestCase):
