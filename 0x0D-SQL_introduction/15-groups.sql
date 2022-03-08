bb3b7a7e51a8c7e21863a924a30883757f05bd17
andresovichh <ahenderson.holberton@gmail.com>
2022-03-08 19:53:21 -0300

    15 & 16

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,7 @@
+-- the number of records with the same score in the table second_table
+-- display the score & nbr of records for this score with label number DESC
+SELECT score
+COUNT(score)
+As number FROM second_table
+GROUP BY score
+ORDER BY number DESC;
