7301d271898c2bb47dfa73b9b3e01944747017a2
andresovichh <ahenderson.holberton@gmail.com>
2022-03-08 19:00:05 -0300

    test 9

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -5,6 +5,7 @@ CREATE TABLE IF NOT EXISTS `second_table` (
     `name` varchar(256),
     `score` int
 );
+-- add rows
 INSERT INTO 'second_table' (id, name)
 VALUES ((1, 2, 3, 4), ('John', 'Alex', 'Bob', 'George'),
 (10, 3, 14, 8))
