5ff45d64dc174b3418073dbef49ada025f9369b3
andresovichh <ahenderson.holberton@gmail.com>
2022-03-08 19:19:29 -0300

    11 test

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,6 @@
+-- all records with a score >= 10 in the table second_table
+-- score and name, ordered by score top first
+SELECT score, name
+FROM second_table
+WHERE score >= 10
+ORDER BY score DESC
