5119db3d59a3c00548d2dfb43449149d70d811a9
andresovichh <ahenderson.holberton@gmail.com>
2022-03-08 19:55:00 -0300

    15

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,7 +1,3 @@
 -- the number of records with the same score in the table second_table
 -- display the score & nbr of records for this score with label number DESC
-SELECT score
-COUNT(score)
-As number FROM second_table
-GROUP BY score
-ORDER BY number DESC;
+SELECT score, COUNT(score) As number FROM second_table GROUP BY score ORDER BY number DESC;
