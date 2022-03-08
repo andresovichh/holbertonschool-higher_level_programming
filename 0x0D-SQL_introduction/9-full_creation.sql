3e21dfd3db09724a471eeef0709dc45f82df2b4c
andresovichh <ahenderson.holberton@gmail.com>
2022-03-08 19:04:52 -0300

    test

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,14 +1,10 @@
 -- create table if doesn't exist
 -- first_table creation
-CREATE TABLE IF NOT EXISTS `second_table` (
-    `id` int,
-    `name` varchar(256),
-    `score` int
-);
+CREATE TABLE IF NOT EXISTS `second_table` (`id` INT, `name` VARCHAR(256), `score` INT);
 -- add rows
 INSERT INTO 'second_table' (`id`, `name`, `score`)
 VALUES
-    (1, 'John', 10)
-    (2, 'Alex', 3)
-    (3, 'Bob', 14)
-    (4, 'George', 8)
+        (1, 'John', 10)
+        (2, 'Alex', 3)
+        (3, 'Bob', 14)
+        (4, 'George', 8)
