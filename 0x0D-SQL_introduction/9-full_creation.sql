69c39a69a5ad95da3b0444c52b91ec2d560d78bf
andresovichh <ahenderson.holberton@gmail.com>
2022-03-08 19:08:51 -0300

    9

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -2,5 +2,9 @@
 -- first_table creation
 CREATE TABLE IF NOT EXISTS `second_table` (`id` INT, `name` VARCHAR(256), `score` INT);
 -- add rows
-INSERT INTO 'second_table' (`id`, `name`, `score`)
-VALUES (1, 'John', 10), (2, 'Alex', 3), (3, 'Bob', 14), (4, 'George', 8);
+INSERT INTO `second_table` (`id`, `name`, `score`)
+VALUES
+	(1, 'John', 10),
+	(2, 'Alex', 3),
+	(3, 'Bob', 14),
+	(4, 'George', 8);
