9986a240f46fc80b42132acec7d891dc5212a2e6
andresovichh <ahenderson.holberton@gmail.com>
2022-03-08 18:26:50 -0300

    fixed 4

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,6 +1,6 @@
 -- create table if doesn't exist
 -- first_table creation
-CREATE TABLE IF NOT EXISTS 'first_table' (
+CREATE TABLE IF NOT EXISTS `first_table` (
     `id` int,
     `name` varchar(256)
 );
