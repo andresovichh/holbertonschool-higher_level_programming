8e77d5067a9397450b2714f78d6752fe4589b62c
andresovichh <ahenderson.holberton@gmail.com>
2022-03-08 19:14:41 -0300

    10

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,5 +1,5 @@
 -- lists all records of the table second_table of the database 
 -- display score and nam, top score first
-SELECT id, name
+SELECT score, name
 FROM second_table
-ORDER BY score DESC
+ORDER BY score DESC;
