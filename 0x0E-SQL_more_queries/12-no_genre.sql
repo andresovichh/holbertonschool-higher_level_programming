475a366f32489bf6bfcd1427fe04c1ca402867fc
andresovichh <ahenderson.holberton@gmail.com>
2022-03-09 20:53:44 -0300

    test 12 bis

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -5,7 +5,7 @@
 
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-JOIN tv_show_genres
+LEFT JOIN tv_show_genres
 ON tv_shows.title=tv_show_genres.name
 WHERE tv_show_genres.show_id is NULL
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
