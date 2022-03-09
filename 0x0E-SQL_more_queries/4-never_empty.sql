6eed5e44e0b40fa3902fa3598239114dee34d2cb
andresovichh <ahenderson.holberton@gmail.com>
2022-03-09 11:42:08 -0300

    test 4

---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,6 @@
+-- creates the table force_name
+-- force_name description: id INT name VARCHAR(256) can’t be null
+CREATE TABLE IF NOT EXISTS `id_not_null`(
+    `id` INT DEFAULT 1,
+    `name` VARCHAR(256) NOT NULL
+);
