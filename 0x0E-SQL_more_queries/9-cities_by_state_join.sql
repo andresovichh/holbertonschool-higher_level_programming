c6dd5924e180c2c123f95b95268a088943d9eccc
andresovichh <ahenderson.holberton@gmail.com>
2022-03-09 16:11:53 -0300

    test 9

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,5 +1,5 @@
 -- list cities in hbtn_0d_usa
 -- asc by cities.id, 1 SELECT
-SELECT id, name
+SELECT cities.id, cities.name, states.name
 FROM cities
 INNER JOIN  states ON cities.state_id = states.id;
