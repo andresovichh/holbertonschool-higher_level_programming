48b2e10797c313aaba064d1eb28419dc3a9aa33f
andresovichh <ahenderson.holberton@gmail.com>
2022-03-09 15:53:05 -0300

    8

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -2,4 +2,4 @@
 -- ascending order
 SELECT id, name
 FROM cities
-WHERE state_id = (SELECT id, FROM states, WHERE name = "California") ORDER BY id ASC;
+WHERE state_id = (SELECT id FROM states WHERE name = "California") ORDER BY id ASC;
