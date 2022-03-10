595ac65df4a641d1b78c6e415fd1c4b6c03dae49
andresovichh <ahenderson.holberton@gmail.com>
2022-03-09 21:48:35 -0300

    13 fix

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -7,7 +7,7 @@
 -- You can use only one SELECT statement
 
 SELECT tv_genres.name AS genre, 
-        COUNT(tv_show_genres.genre_id) AS number_of_rows
+        count(tv_show_genres.genre_id) AS number_of_shows
 FROM tv_genres
         INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY tv_genres.name
