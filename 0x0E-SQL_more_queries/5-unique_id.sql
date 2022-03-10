49e3ca8ba1c5bc6642c8d90782b7c71a5d9ef059
andresovichh <ahenderson.holberton@gmail.com>
2022-03-09 21:02:02 -0300

    fix 5

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,6 +1,6 @@
 -- creates the table force_name
 -- force_name description: id INT name VARCHAR(256) can’t be null
-CREATE TABLE IF NOT EXISTS `id_not_null`(
+CREATE TABLE IF NOT EXISTS `unique_id`(
     `id` INT UNIQUE DEFAULT 1,
     `name` VARCHAR(256)
 );
