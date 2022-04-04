68433a9232cc1ea0bea6dd8afd2b5d5dea159dcf
andresovichh <ahenderson.holberton@gmail.com>
2022-04-04 20:19:38 -0300

    test 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const j = ['C is fun ', 'Python is cool ', 'Javascript is amazing '];
+const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let i = 0; i < j.length; i++) {
   console.log(j[i]);
 }
