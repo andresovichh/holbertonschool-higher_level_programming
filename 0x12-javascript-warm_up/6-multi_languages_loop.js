a0fb23bba8bddbc632371d026c3849d85c058de4
andresovichh <ahenderson.holberton@gmail.com>
2022-04-04 20:09:29 -0300

    test 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const Arr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const Arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 let i = 0;
 while (i < 3) {
   console.log(Arr[i]);
