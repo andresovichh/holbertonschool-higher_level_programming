435fc1180b8e71a77b081cbb60e6239e7f5f33d5
andresovichh <ahenderson.holberton@gmail.com>
2022-04-04 20:55:44 -0300

    test

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,10 @@
+#!/usr/local/bin/node
+if (process.argv[2] === undefined || isNaN(process.argv[2])) {
+  console.log('Missing size');
+} else if (parseInt(process.argv[2] < 1)) {
+
+} else {
+  for (let i = 0; i < parseInt(process.argv[2]); i++) {
+    console.log('X'.repeat(parseInt(process.argv[2])));
+  }
+}
