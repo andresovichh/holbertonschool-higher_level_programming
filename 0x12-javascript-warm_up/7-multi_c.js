737e2915ca1f560e8a62ffe62658bde094ba36a4
andresovichh <ahenderson.holberton@gmail.com>
2022-04-04 20:36:04 -0300

    test 7

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,12 +1,8 @@
-#!/usr/bin/node
-if (process.argv[2] !== undefined) {
-  if (parseInt(process.argv[2])) {
-    for (let i = 0; i <= parseInt(process.argv[2]) - 1; i++) {
-      console.log('C is fun');
-    }
-  } else {
-    console.log('Missing number of occurrences');
-  }
-} else {
+#!/usr/local/bin/node
+if (process.argv[2] === undefined || isNaN(process.argv[2])) {
   console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i <= parseInt(process.argv[2]) - 1; i++) {
+    console.log('C is fun');
+  }
 }
