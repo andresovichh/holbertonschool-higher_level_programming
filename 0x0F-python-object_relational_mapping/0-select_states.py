37c056fb5d410fd5691a6dca0e187af70792bdb6
andresovichh <ahenderson.holberton@gmail.com>
2022-04-07 18:47:02 -0300

    saving progress

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,4 +7,6 @@ import sys
 
 """ should be guarded"""
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port="3306", user=sys.argv[1], passwd=sys.arv[2], db=sys.argv[3])
+    db = MySQLdb.connect(host="localhost", port="3306", user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM {} ORDER BY ASC".format(sys.argv[3]))
