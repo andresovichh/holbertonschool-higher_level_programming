706ded7f92dfa67591ad18f6ce7617c760967003
andresovichh <ahenderson.holberton@gmail.com>
2022-04-07 22:21:54 -0300

    1 sems to work fine

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+list all states from the database hbtn_0e_0_usa
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    """ the guard """
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    result = cursor.fetchall()
+    for row in result:
+        print(row)
