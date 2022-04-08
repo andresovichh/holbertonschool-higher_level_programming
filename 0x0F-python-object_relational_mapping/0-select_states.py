7e5fa4ab446e9145d0d7eae23f0cb6b09526eb79
andresovichh <ahenderson.holberton@gmail.com>
2022-04-07 21:59:31 -0300

    i think i hve 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
 """
-Module that lists all states from the database hbtn_0e_0_usa 
+list all states from the database hbtn_0e_0_usa
 """
+
 import MySQLdb
 import sys
 
-""" should be guarded"""
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port="3306", user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    cur = db.cursor()
-    cur.execute("SELECT * FROM {} ORDER BY ASC".format(sys.argv[3]))
+    """ the guard """
+    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    result = cursor.fetchall()
+    for row in result:
+        print(row)
