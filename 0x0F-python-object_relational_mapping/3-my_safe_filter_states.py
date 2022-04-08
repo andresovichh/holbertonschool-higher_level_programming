66972638bbb7e857e94c10484edc59d90b672081
andresovichh <ahenderson.holberton@gmail.com>
2022-04-08 16:36:30 -0300

    all qorking up to three

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -13,8 +13,8 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                          passwd=sys.argv[2], db=sys.argv[3], charset="utf8")
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name='{}' \
-                   ORDER BY id ASC".format(sys.argv[4]))
+    cursor.execute("""SELECT * FROM states WHERE name= %(name)s \
+                   ORDER BY id ASC""", {'name': sys.argv[4]})
     result = cursor.fetchall()
     for row in result:
         print(row)
