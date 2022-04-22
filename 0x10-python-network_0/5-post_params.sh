fe0a0b9918e4bee52885e9bd7ab47b4239ab200d
andresovichh <ahenderson.holberton@gmail.com>
2022-04-22 15:41:03 -0300

    fix 5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 # Write a Bash script that takes in a URL, sends a POST request to the passed URL, and displays the body of the response
-# A variable email must be sent with the value test@gmail.com
-# A variable subject must be sent with the value I will always be here for PLD
 curl -s -X POST -H "Content-Type: application/json" -d '{"email":"test@gmail.com","subject":"I will always be here for PLD"}' "$1"
