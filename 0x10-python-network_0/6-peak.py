b0c5faee9387a16f499c7a23f31687243869dbbf
andresovichh <ahenderson.holberton@gmail.com>
2022-04-22 16:09:28 -0300

    test

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+this is a function to find the peaks"""
+
+sorted_list = []
+def find_peak(list_of_integers):
+    """ this is a foo that finds a peak"""
+    if len(list_of_integers) == 0:
+        return None
+    else:
+        sorted_list = sorted(list_of_integers)
+
+        return (sorted_list[len(sorted_list) - 1]) 
