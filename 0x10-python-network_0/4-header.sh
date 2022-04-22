cbdc38c1ac2c2b19e82513a9adba0a3477a3b59a
andresovichh <ahenderson.holberton@gmail.com>
2022-04-22 15:26:25 -0300

    test 4

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # takes URL as argumen, sends a GET reuest to the URL and displays the body of the response
-curl -sH "X-HolbertonSchool-User-Id=98" "$1"
+curl -sH "X-HolbertonSchool-User-Id:98" "$1"
