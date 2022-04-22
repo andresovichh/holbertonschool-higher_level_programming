38155e71215f50e601c8f9f4c6dee5b16df8483a
andresovichh <ahenderson.holberton@gmail.com>
2022-04-22 14:17:30 -0300

    test 0

---
+++ tests/sum-main.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+sum = 0
+arr = [1000000001, 1000000002, 1000000003, 1000000004, 1000000005]
+for i in arr:
+    sum += i
+print(sum)
---
+++ tests/tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+sum = 0
+arr = [1 , 2, 3, 4, 5]
+sum = sum(arr)
+print(sum)
