46e5944ba5673ec0e1b2d4393a499102871d1d10
andresovichh <ahenderson.holberton@gmail.com>
2022-04-23 12:35:37 -0300

    fixes 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,11 +4,9 @@ This is a module that fetches a URL"""
 
 import urllib.request
 
-
-
 with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
     response = response.read()
     print("Body response:")
-    print("\t- {}".format(type(response)))
-    print("\t- {}".format(response))
-    print("\t- {}".format(response.decode('utf-8')))
+    print("\t- type: {}".format(type(response)))
+    print("\t- content: {}".format(response))
+    print("\t- utf8 content: {}".format(response.decode('utf-8')))
