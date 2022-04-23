5f783a39e4245c3b2ea3b6812beafe64524cdeb7
andresovichh <ahenderson.holberton@gmail.com>
2022-04-22 23:09:15 -0300

    test 4

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -8,10 +8,8 @@ this is a function to find the peaks"""
 def find_peak(list_of_integers):
     """ this is a foo that finds a peak"""
 
-    sorted_list = []
-    if len(list_of_integers) == 0:
-        return None
-    else:
-        sorted_list = sorted(list_of_integers)
-
-        return (sorted_list[len(sorted_list) - 1])
+    peak = 0
+    for i in list_of_integers:
+        if (i + 1) > i and (i + 1) < (i+ 2):
+            peak = i
+    return peak
