c27d714a9fa77341024736d711a0c2b1035ec3cb
andresovichh <ahenderson.holberton@gmail.com>
2022-04-23 15:08:22 -0300

    test 6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -19,6 +19,6 @@ if __name__ == '__main__':
     import requests
     from sys import argv
 
-    payload = {'email':str(argv[2])}
-    r = requests.get(argv[1], data=payload)
+    payload = {'email': argv[2]}
+    r = requests.post(argv[1], data=payload)
     print(r.text)
