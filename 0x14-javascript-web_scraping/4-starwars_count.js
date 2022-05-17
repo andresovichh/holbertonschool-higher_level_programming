9c4865819c2fe5e140cbcf003941c8579958dc9f
andresovichh <ahenderson.holberton@gmail.com>
2022-05-17 16:10:11 -0300

    test 4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const argv = process.argv;
+const axios = require('axios').default;
+const apii = (argv[2]);
+axios.get(apii)
+  .then(function (response) {
+    let counter = 0;
+
+    for (const film in response.data.results) {
+        if (response.data.results[film].characters.includes('18')) {
+            counter++;
+        }
+    console.log(counter);
+  })
+  .catch(function (error) {
+    console.log(error.response.status);
+  });
