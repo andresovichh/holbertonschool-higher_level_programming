75ea4bcd9939ec93cfcca28626e764c9beac4984
andresovichh <ahenderson.holberton@gmail.com>
2022-05-17 15:41:03 -0300

    test 3

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const argv = process.argv;
 const axios = require('axios').default;
-const apii = 'https://swapi-api.hbtn.io/api/films/'.concat(argv[2])
+const apii = 'https://swapi-api.hbtn.io/api/films/'.concat(argv[2]);
 axios.get(apii)
   .then(function (response) {
     console.log(response.data.title);
