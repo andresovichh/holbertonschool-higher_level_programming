070a87c206bdac081a1ed00cb2f9b0719173d995
andresovichh <ahenderson.holberton@gmail.com>
2022-05-17 16:23:33 -0300

    test 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,18 +1,18 @@
 #!/usr/bin/node
-
 const argv = process.argv;
 const axios = require('axios').default;
-const apii = (argv[1]);
+const apii = (argv[2]);
 axios.get(apii)
   .then(function (response) {
     let counter = 0;
 
     for (const film in response.data.results) {
-        if (response.data.results[film].characters.includes('18')) {
-            counter++;
-        }
+      if (response.data.results[film].characters.includes('https://swapi-api.hbtn.io/api/people/18/')) {
+        counter = counter + 1;
+      }
+    }
     console.log(counter);
-  }})
+  })
   .catch(function (error) {
     console.log(error.response.status);
   });
