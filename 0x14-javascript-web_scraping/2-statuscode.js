fa7f5e75909196da8252131144d903ff2fdf8839
andresovichh <ahenderson.holberton@gmail.com>
2022-05-17 15:11:08 -0300

    tets 2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,8 +1,13 @@
 #!/usr/bin/node
 
-const req = require('axios').default;
+/*
+This is a module
+that does something
+*/
 const argv = process.argv;
-req.get(argv[2])
+const axios = require('axios').default;
+
+axios.get(argv[2])
     .then(function (response) {
         console.log('code: ' + response.statusCode);
     })
