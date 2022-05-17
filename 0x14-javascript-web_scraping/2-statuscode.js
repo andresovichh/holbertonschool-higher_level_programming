e517853b535f36ba8ff76d76480a19b7eaffe330
andresovichh <ahenderson.holberton@gmail.com>
2022-05-17 15:16:57 -0300

    test 2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -8,9 +8,9 @@ that does something
 const axios = require('axios').default;
 
 axios.get(process.argv[2])
-    .then(function (response, error) {
-        console.log('code: ' + response.status);
-    })
-    .catch(function (error) {
-        console.log('code: ' + error.response.status);
-    });
+  .then(function (response, error) {
+    console.log('code: ' + response.status);
+  })
+  .catch(function (error) {
+    console.log('code: ' + error.response.status);
+  });
