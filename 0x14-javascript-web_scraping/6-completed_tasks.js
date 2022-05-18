be98a80641d0783db383fca31c7ab290d1236a8f
andresovichh <ahenderson.holberton@gmail.com>
2022-05-17 22:06:31 -0300

    test 6

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,25 @@
+#!/usr/local/bin/node
+
+/*
+script that computes the number of
+tasks completed by user id for each user
+*/
+const axios = require('axios').default;
+const argv = process.argv;
+const apii = argv[2];
+
+axios.get(apii)
+    .then(function (response) {
+        const dict = {};
+        /* console.log(response.data); */
+        for (const x in response.data) {
+            const user = response.data[x].userId;
+            const ended = response.data[x].completed;
+            if (isNaN(dict[user]) && ended) {
+              dict[user] = 1;
+            } else if (ended) {
+              dict[user] += 1;
+            }
+          }
+          console.log(dict);
+        });
